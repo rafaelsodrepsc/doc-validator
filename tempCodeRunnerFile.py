bfdcab2a44f30ba8a3dba9116ae07333e465a5eb
Rafael <rafaelsodrepsc@gmail.com>
2026-05-13 01:17:00 -0300

    chore: remove temp file

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from services import extract,ocr_service,preprocess
-
-image_path = 'samples/documento.png'
-
-pre_processment = preprocess(image_path)
-
-ocr = ocr_service(pre_processment)
-
-print(extract(ocr))
