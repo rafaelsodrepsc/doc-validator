6fedd55d193391ea9012dea32b6aac143a4121f6
Rafael <rafaelsodrepsc@gmail.com>
2026-05-13 00:30:16 -0300

    feat: improve image quality and fix ocr extraction

--- samples/gerar_documento.py
+++ samples/gerar_documento.py
@@ -1,12 +1,14 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw,ImageFont
+
+font = ImageFont.truetype("arial.ttf", size=24)
 
 img = Image.new('RGB',(400,200), color=(255,255,255))
 
 draw = ImageDraw.Draw(img)
 
-draw.text((50,50), "Nome: Rafael Sodre Paschoal",fill=(0,0,0))
-draw.text((50,70), "CPF: 123.456.789-09",fill=(0,0,0))
-draw.text((50,90), "Data de Nascimento: 09/09/2003",fill=(0,0,0))
+draw.text((50,50), "Nome: Rafael Sodre Paschoal",fill=(0,0,0),font=font)
+draw.text((50,90), "CPF: 123.456.789-09",fill=(0,0,0),font=font)
+draw.text((50,130), "Data de Nascimento: 09/09/2003",fill=(0,0,0),font=font)
 
 img.show()
-img.save('documento.png')
+img.save('samples/documento.png')
--- services/preprocessor.py
+++ services/preprocessor.py
@@ -1,6 +1,7 @@
 import cv2
 
 def pre_process(image_path):
+    
     image = cv2.imread(image_path)
 
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
